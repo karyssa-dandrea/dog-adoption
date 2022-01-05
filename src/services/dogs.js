8c0c9145fa49a80f07aeb4562a5637cdd6ccac12
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-05 12:57:57 -0800

    aded my insertDog function

--- src/services/dogs.js
+++ src/services/dogs.js
@@ -1,17 +1,16 @@
 import { checkError, client } from './client';
 
 export async function fetchDogs() {
-  const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/dogs?`, {
-    headers: {
-      apikey: process.env.REACT_APP_SUPABASE_KEY,
-      Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-    },
-  });
-  const data = await resp.json();
-  return data;
+  const resp = await client.from('dogs').select('*').order('name');
+  return checkError(resp);
 }
 
 export async function fetchDogById(id) {
   const resp = await client.from('dogs').select('*').match({ id }).single();
+  return checkError(resp);
+}
+
+export async function insertDog(dog) {
+  const resp = await client.from('dogs').insert(dog);
   return checkError(resp);
 }
