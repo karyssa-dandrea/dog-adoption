dd5074da5b40881c72ff5ba1356a6e919a51b44d
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-05 12:38:08 -0800

    added css

--- src/components/DogDeets/DogList.js
+++ src/components/DogDeets/DogList.js
@@ -4,21 +4,16 @@ import './DogDetail.css';
 
 export default function DogList({ dogs }) {
   return (
-    <>
-      <div className="dogs-list">
-        <div className="container">
-          {dogs.map((dogs) => (
-            <Link className="lists" key={dogs.id} to={`/dogs/${dogs.id}`}>
-              <h1>Meet {dogs.name}</h1>
-              <img src={dogs.image}></img>
-              <p>
-                {' '}
-                {dogs.name} is a(n) {dogs.age} year old {dogs.breed}
-              </p>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </>
+    <div className="container">
+      {dogs.map((dogs) => (
+        <Link className="lists" key={dogs.id} to={`/dogs/${dogs.id}`}>
+          <h1>Meet {dogs.name}</h1>
+          <img src={dogs.image}></img>
+          <p>
+            {dogs.name} is a(n) {dogs.age} year old {dogs.breed}
+          </p>
+        </Link>
+      ))}
+    </div>
   );
 }
