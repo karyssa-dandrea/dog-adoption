20342fc670b85204b3e131eee9c019ff352f1702
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-05 17:09:14 -0800

    added my dog form page and info and my button

--- src/components/Form/Dogform.js
+++ src/components/Form/Dogform.js
@@ -1,5 +1,59 @@
 import React from 'react';
 
-export default function Dogform() {
-  return <form></form>;
+export default function Dogform({ name, bio, age, image, breed, buttonHandler, updateDogState }) {
+  return (
+    <form>
+      <h2>Add Dog Details</h2>
+      <div className="dog-form">
+        <label>Name</label>
+        <input
+          placeholder="name"
+          type="text"
+          value={name}
+          onChange={(e) => {
+            updateDogState(e.target.value);
+          }}
+        />
+        <label>Bio:</label>
+        <input
+          placeholder="bio"
+          type="text"
+          value={bio}
+          onChange={(e) => {
+            updateDogState(e.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          placeholder="age"
+          type="text"
+          value={age}
+          onChange={(e) => {
+            updateDogState(e.target.value);
+          }}
+        />
+        <label>Image:</label>
+        <input
+          placeholder="image"
+          type="text"
+          value={image}
+          onChange={(e) => {
+            updateDogState(e.target.value);
+          }}
+        />
+        <label>Breed:</label>
+        <input
+          placeholder="breed"
+          type="text"
+          value={breed}
+          onChange={(e) => {
+            updateDogState(e.target.value);
+          }}
+        />
+      </div>
+      <div className="button">
+        <button onClick={buttonHandler}>Add</button>
+      </div>
+    </form>
+  );
 }
