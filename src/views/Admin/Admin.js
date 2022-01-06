4c98995631cd83b71f9b9d8cc927f7777b07007b
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-05 16:06:18 -0800

    added my stat to admin and update

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
+import { insertDog } from '../../services/dogs';
+import DogForm from '../../components/Form/Dogform';
 
 export default function Admin() {
   const [dog, setDog] = useState({});
 
-  const 
+  const buttonHandler = async () => {
+    await insertDog(dog);
+    setDog({});
+  };
+
+  const updateDogState = (key, value) => {
+    dog[key] = value;
+    setDog({ ...dog });
+  };
+
   return (
-    <div>
-      <h1>Admin Page</h1>
+    <div className="admin">
+      <DogForm buttonHandler={buttonHandler} {...dog} updateDogState={updateDogState} />
     </div>
   );
 }
