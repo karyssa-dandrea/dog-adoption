18e3134ee576bec00cc5efee813253a2beb7e77b
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-07 15:06:54 -0800

    updated my dogs test to pass

--- src/views/Edit/DogEdit.test.js
+++ src/views/Edit/DogEdit.test.js
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import DogEdit from '../../views/Edit/DogEdit';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 test('should render dog details in a form', async () => {
   const { container } = render(
-    <MemoryRouter>
-      <DogEdit />
+    <MemoryRouter initialEntries={['/dogs/4/edit']}>
+      <Route path="/dogs/:id/edit" component={DogEdit} />
     </MemoryRouter>
   );
-  await screen.findByText('Edit Dog');
+  await screen.findByDisplayValue('Maggie');
   expect(container).toMatchSnapshot();
 });
